8bd57554dacf931060f71812a305e8e61fc24fc1
EmersonBraun <emersonfbraun@gmail.com>
2026-04-02 19:51:08 -0300

    feat: wire up core exports and main entry point

---
+++ src/core/index.ts
@@ -0,0 +1,18 @@
+export { useStream } from './useStream'
+export { useReactive } from './useReactive'
+export { useChat } from './useChat'
+export type {
+  StreamStatus,
+  MessageRole,
+  MessageStatus,
+  ToolCallStatus,
+  ToolCall,
+  Message,
+  StreamChunk,
+  StreamSource,
+  UseStreamOptions,
+  UseStreamReturn,
+  ChatConfig,
+  ChatReturn,
+  AdapterFactory,
+} from './types'
